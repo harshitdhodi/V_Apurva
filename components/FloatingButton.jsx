fb37ea6a4628d9ac77d43462d1b17020a5fd1f74
harshitdhodi <harshit.dhodi2108@gmail.com>
2025-12-19 12:05:06 +0530

    update whatsapp icon

--- components/FloatingButton.jsx
+++ components/FloatingButton.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import ContactUsInquiryForm from './ContactUsInquiryForm';
+import { BsWhatsapp } from 'react-icons/bs';
 
 const FloatingButtons = () => {
   const [showInquiryForm, setShowInquiryForm] = useState(false);
@@ -35,13 +36,7 @@ const FloatingButtons = () => {
           className="bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition transform hover:scale-110"
           aria-label="Chat on WhatsApp"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            className="h-8 w-8 fill-current"
-          >
-            <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 108.1 27h.1c122.3 0 222.1-99.6 222.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6z" />
-          </svg>
+         <BsWhatsapp className="w-8 h-8" />
         </a>
            </div>
 
